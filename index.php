d12e2fc615b405ccd65f2dd9b1d920424bedde80
Tsyf <245777191+Tsyf02@users.noreply.github.com>
2026-05-03 14:03:34 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="id" data-theme="light">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="author" content="Talitha Syifa Al Fath_124250173 & Marva H._124250159">    
+  <meta name="description" content="SIPemandiri - Sistem Informasi Pemetaan Lokasi & Data Mandiri Penduduk Non-Permanen (Kontrak & Kost) Berbasis Web"> 
