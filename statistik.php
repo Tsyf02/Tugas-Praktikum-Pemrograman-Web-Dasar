fa371b48702391e51a077ff4920354e0b329d244
Tsyf <245777191+Tsyf02@users.noreply.github.com>
2026-05-04 14:25:21 +0700

    Create statistik.php

---
+++ statistik.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+
+<!-- <style> -->
+
+
+</head>
+<body>
+    
+<header>
+
+<div class="logo">
+📍 SiPemandiri
+</div>
+<nav>
+<a href="index.php">Beranda</a>
+<a href="pendataan.php">Pendataan</a>
+<a href="pemetaan.php">Pemetaan</a>
+<a href="statistik.php">Statistik</a>
+<a href="tentang.php">Tentang</a>
+</nav>
+</header>
+
+</body>
+</html>
+
+
