9cf36392643e8b05d6f88e41c7f71d97b694a9b8
marvaa <hervianvian171@gmail.com>
2026-05-04 10:20:54 +0700

    Add files via upload

---
+++ pemetaan.php
@@ -0,0 +1,65 @@
+<!-- ========================= -->
+<!-- FILE : pemetaan.php -->
+<!-- ========================= -->
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Pemetaan</title>
+
+<link rel="stylesheet" href="style.css">
+
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<header>
+
+<div class="logo">
+📍 SiMap Kost & Kontrak
+</div>
+
+<nav>
+<a href="index.php">Beranda</a>
+<a href="pendataan.php">Pendataan</a>
+<a href="pemetaan.php">Pemetaan</a>
+<a href="tentang.php">Tentang</a>
+</nav>
+
+</header>
+
+<section class="hero">
+
+<h1>
+Pemetaan Lokasi Penduduk
+</h1>
+
+<p>
+Halaman ini digunakan untuk melihat
+persebaran lokasi penghuni kost dan kontrakan.
+</p>
+
+<div class="map-box">
+
+<iframe
+src="https://maps.google.com/maps?q=depok&t=&z=13&ie=UTF8&iwloc=&output=embed"
+width="100%"
+height="450"
+style="border:0; border-radius:20px; margin-top:30px;">
+</iframe>
+
+</div>
+
+</section>
+
+<footer>
+© 2026 Sistem Informasi Penduduk Non-Permanen
+</footer>
+
+</body>
+</html>
