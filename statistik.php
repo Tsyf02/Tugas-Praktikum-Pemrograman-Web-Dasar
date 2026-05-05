b18e20bc165e6d5f189c3662fbd890eddd41ef5c
Tsyf <245777191+Tsyf02@users.noreply.github.com>
2026-05-06 01:52:31 +0700

    Update metadata and header title in statistik.php

--- statistik.php
+++ statistik.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+if(!isset($_SESSION['login'])){ header("Location: login.php"); exit; }
+
 $conn = mysqli_connect("localhost","root","","db_penduduk");
 
 $total = mysqli_fetch_assoc(mysqli_query($conn,"
@@ -18,13 +20,15 @@ SELECT COUNT(*) as jumlah FROM penduduk WHERE jenis='Kontrak'
 <!DOCTYPE html>
 <html lang="id">
 <head>
-
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<title>Statistik</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="author" content="Talitha Syifa Al Fath_124250173 & Marva H._124250159">    
+<meta name="description" content="SIPemandiri - Sistem Informasi Pemetaan Lokasi & Data Mandiri Penduduk Non-Permanen (Kontrak & Kost) Berbasis Web"> 
+<title>SIPeManDiRi</title>
+<link rel="icon" href="LOGOSIPEMANDIRI.png" type="image/x-icon"> 
 
 <link rel="stylesheet" href="style.css">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
@@ -99,10 +103,11 @@ width:100%;
 <header>
 
 <div class="logo">
-📊 Statistik
+📍 SIPeManDiRi
 </div>
 
 <nav>
+<a href="logout.php">Logout</a>
 <a href="index.php">Beranda</a>
 <a href="pendataan.php">Pendataan</a>
 <a href="pemetaan.php">Pemetaan</a>
