888f6d3a225aa949ae5fcf399e462d8f346eca7f
Tsyf <245777191+Tsyf02@users.noreply.github.com>
2026-05-06 01:49:28 +0700

    Implement session check and add meta tags
    Added session management and author meta tags.

--- pendataan.php
+++ pendataan.php
@@ -1,4 +1,7 @@
 <?php
+session_start();
+if(!isset($_SESSION['login'])){ header("Location: login.php"); exit; }
+
 
 $conn = mysqli_connect("localhost","root","","db_penduduk");
 
@@ -11,10 +14,11 @@ $data = mysqli_query($conn,"SELECT * FROM penduduk");
 <head>
 
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<title>Pendataan</title>
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="author" content="Talitha Syifa Al Fath_124250173 & Marva H._124250159">    
+<meta name="description" content="SIPemandiri - Sistem Informasi Pemetaan Lokasi & Data Mandiri Penduduk Non-Permanen (Kontrak & Kost) Berbasis Web"> 
+<title>SIPeManDiRi</title>
+<link rel="icon" href="LOGOSIPEMANDIRI.png" type="image/x-icon"> 
 <link rel="stylesheet" href="style.css">
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
@@ -29,6 +33,7 @@ $data = mysqli_query($conn,"SELECT * FROM penduduk");
 </div>
 
 <nav>
+<a href="logout.php">Logout</a>
 <a href="index.php">Beranda</a>
 <a href="pendataan.php">Pendataan</a>
 <a href="pemetaan.php">Pemetaan</a>
