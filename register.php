0b08eeb51d59a5cc0ee4e17dde06ef1980c6d87f
Tsyf <245777191+Tsyf02@users.noreply.github.com>
2026-05-05 23:37:52 +0700

    Update register.php

--- register.php
+++ register.php
@@ -30,7 +30,9 @@ if(isset($_POST['daftar'])){
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Talitha Syifa Al Fath_124250173 & Marva H._124250159">    
+    <meta name="description" content="SIPemandiri - Sistem Informasi Pemetaan Lokasi & Data Mandiri Penduduk Non-Permanen (Kontrak & Kost) Berbasis Web"> 
     <title>Daftar Akun</title>
     <style>
         *{margin:0;padding:0;box-sizing:border-box;font-family:'Poppins',sans-serif;}
