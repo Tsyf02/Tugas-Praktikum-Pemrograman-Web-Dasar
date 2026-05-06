4148d32ef87aeba100f51ec417b51e2300ea1049
Tsyf <245777191+Tsyf02@users.noreply.github.com>
2026-05-06 16:47:37 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,3 +5,22 @@ session_destroy();
 header("Location: login.php");
 exit;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="author" content="Talitha Syifa Al Fath_124250173 & Marva H._124250159">    
+<meta name="description" content="SIPemandiri - Sistem Informasi Pemetaan Lokasi & Data Mandiri Penduduk Non-Permanen (Kontrak & Kost) Berbasis Web"> 
+<title>SIPeManDiRi</title>
+<link rel="icon" href="LOGOSIPEMANDIRI.png" type="image/x-icon"> 
+</head>
+<body>
+    
+
+<br>
+<footer>
+© 2026 Sistem Informasi Penduduk Non-Permanen
+</footer>
+</body>
+</html>
